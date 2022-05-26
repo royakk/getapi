8d24933ae8bc098778c553683f969da39a86baf2
roya <royakarimi9069@gmail.com>
2022-05-26 23:58:45 +0430

    2

--- src/get.jsx
+++ src/get.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './st.css';
 
 export default class Get extends Component {
     state ={
@@ -16,12 +17,19 @@ export default class Get extends Component {
     return (
       
       <div className='container'>
+        <div >
+        <input className="button-3" type='button' onClick={this.creatUser} value="create" />
+        </div>
         {this.state.users.map((users)=>{
           return (
           <div className="users" style={{display:'contents'}}>
               <img src={users.avatar} style={{width:'100px' ,borderRadius:'50%'}}/>
               <h1>{users.first_name} {users.last_name}</h1>
               <h2>{users.email}</h2>
+              <div>
+              <input className='button-1' type='button' value="update" onClick={this.hdlupdate}/>
+              <input className='button-1'  type='button' value="delete" onClick={this.hdldelete} />
+              </div>
           </div>
           );
         })}
@@ -29,5 +37,27 @@ export default class Get extends Component {
       )
     
       }
-  
+   hdlupdate = async(users)=>{
+     const newupdate =await axios.put(`https://reqres.in/api/users/${users.id}`,users)
+     this.state.users.last_name="roya"
+     this.setState({users : newupdate.data.users}) 
+    }
+    hdldelete = async(users)=>{
+      const deletuser= await axios.delete(`https://reqres.in/api/users/${users.id}`)
+      console.log(deletuser)
+      const indexuser =this.state.users.filter ((u) =>u.id !==users.id)
+      this.setState ({users : indexuser})
+    }
+
+    creatUser = async()=>{
+      const newuser= {
+        first_name:'roya',
+        last_name :'karimi',
+        email : 'bhjkjhlkjlkj'
+      
+    }
+    const response= await axios.post ('https://reqres.in/api/users',newuser)
+    console.log(response)
+  }
+
 }
